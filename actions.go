755638ea11f2a6d5053ba2199c45f4f493c3338c
nico <brun.nicolas21@gmail.com>
2019-07-07 18:58:43 +0200

    split main files

--- actions.go
+++ actions.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"time"
+	"text/template"
+	"os"
+	"os/exec"
+	"unicode/utf8"
+	"strconv"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"os"
-	"os/exec"
-	"strconv"
 	"strings"
-	"text/template"
-	"time"
-	"unicode/utf8"
 
 	"golang.org/x/sync/errgroup"
 )
@@ -287,47 +287,4 @@ func (a Action) prepareScript() (string, error) {
 		}
 	}
 	return b.String(), nil
-}
-
-func ParseShell(str string) []string {
-	const (
-		single byte = '\''
-		double      = '"'
-		space       = ' '
-		equal       = '='
-	)
-
-	skipN := func(b byte) int {
-		var i int
-		if b == space || b == single || b == double {
-			i++
-		}
-		return i
-	}
-
-	var (
-		ps  []string
-		j   int
-		sep byte = space
-	)
-	for i := 0; i < len(str); i++ {
-		if str[i] == sep || str[i] == equal {
-			if i > j {
-				j += skipN(str[j])
-				ps, j = append(ps, str[j:i]), i+1
-				if sep == single || sep == double {
-					sep = space
-				}
-			}
-			continue
-		}
-		if sep == space && (str[i] == single || str[i] == double) {
-			sep, j = str[i], i+1
-		}
-	}
-	if str := str[j:]; len(str) > 0 {
-		i := skipN(str[0])
-		ps = append(ps, str[i:])
-	}
-	return ps
 }
