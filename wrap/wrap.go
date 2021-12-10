6c6d8c22b8c298a2d709b151724d8cf136cc60ed
midbel <brun.nicolas21@gmail.com>
2021-12-10 17:06:46 +0100

    wrap package

--- wrap/wrap.go
+++ wrap/wrap.go
@@ -1,11 +1,78 @@
 package wrap
 
-const MaxLenght = 72
+import (
+	"strings"
+	"unicode/utf8"
+)
+
+const DefaultLength = 70
+
+const (
+	space = ' '
+	tab   = '\t'
+	nl    = '\n'
+)
 
 func Wrap(str string) string {
-  return WrapN(str, MaxLength)
+	return WrapN(str, DefaultLength)
 }
 
 func WrapN(str string, n int) string {
-  return str
+	if len(str) < n {
+		return str
+	}
+	var (
+		ws  strings.Builder
+		ptr int
+	)
+	for i := 0; ptr < len(str); i++ {
+		if i > 0 {
+			ws.WriteRune(nl)
+		}
+		x := advance(str[ptr:], n)
+		if x == 0 {
+			break
+		}
+		ws.WriteString(strings.TrimSpace(str[ptr : ptr+x]))
+		ptr += x
+	}
+	return ws.String()
+}
+
+func advance(str string, n int) int {
+	if len(str) == 0 {
+		return 0
+	}
+	var (
+		curr int
+		prev int
+	)
+	for {
+		r, z := utf8.DecodeRuneInString(str[curr:])
+		if r == utf8.RuneError {
+			break
+		}
+		curr += z
+		if isNL(r) {
+			break
+		}
+		if isBlank(r) {
+			if curr == n {
+				break
+			} else if curr > n {
+				curr = prev
+				break
+			}
+			prev = curr
+		}
+	}
+	return curr
+}
+
+func isBlank(r rune) bool {
+	return r == space || r == tab
+}
+
+func isNL(r rune) bool {
+	return r == nl
 }
---
+++ wrap/wrap_test.go
@@ -0,0 +1,56 @@
+package wrap_test
+
+import (
+	"bufio"
+	"strings"
+	"testing"
+
+	"github.com/midbel/maestro/wrap"
+)
+
+func TestWrapN(t *testing.T) {
+	data := []struct {
+		Input string
+		Len   int
+	}{
+		{
+			Input: "the quick brown fox jumps over the lazy dog",
+			Len:   15,
+		},
+		{
+			Input: "the quick brown fox jumps over the lazy dog",
+			Len:   20,
+		},
+		{
+			Input: "the quick brown fox jumps over the lazy dog",
+			Len:   30,
+		},
+		{
+			Input: `simple is a sample maestro file that can be used for any Go project.
+It provides commands to automatize building, checking and testing
+your Go project.
+
+It has also some commands to give statistics on the status of the
+project such as number of remaining todos, line of codes and others.`,
+			Len: 70,
+		},
+	}
+	for _, d := range data {
+		var (
+			got  = wrap.WrapN(d.Input, d.Len)
+			scan = bufio.NewScanner(strings.NewReader(got))
+		)
+		if len(got) == 0 && len(d.Input) > 0 {
+			t.Errorf("nothing has been wrapped!")
+			continue
+		}
+		for scan.Scan() {
+			str := scan.Text()
+			if len(str) > d.Len {
+				t.Errorf("%s: longer than expected! want %d, got %d", str, d.Len, len(str))
+				break
+			}
+			t.Logf("%2d(%d): %s", len(str), d.Len, str)
+		}
+	}
+}
