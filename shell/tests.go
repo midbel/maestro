ae68db49d7ec2eb2686c68ea14d9129c3f210457
midbel <brun.nicolas21@gmail.com>
2021-12-29 21:34:35 +0100

    binary and unary operators for standard expander

--- shell/tests.go
+++ shell/tests.go
@@ -78,75 +78,85 @@ func (t UnaryTest) Test(env Environment) (bool, error) {
 	case FileDir:
 		return t.fileDirectory(env)
 	case StrNotEmpty:
+		str, err := expandSingle(t.Right, env)
+		if err != nil {
+			return false, err
+		}
+		return str != "", nil
 	case StrEmpty:
+		str, err := expandSingle(t.Right, env)
+		if err != nil {
+			return false, err
+		}
+		return str == "", nil
 	default:
+		return false, fmt.Errorf("unknown/unsupported unary test operator")
 	}
-	return false, ErrTest
 }
 
 func (t UnaryTest) fileExists(env Environment) (bool, error) {
-	return statFileWith(t.Right, env, func(_ os.FileInfo) (bool, error) {
-		return true, nil
+	return statFileWith(t.Right, env, func(_ os.FileInfo) bool {
+		return true
 	})
 }
 
 func (t UnaryTest) fileReadable(env Environment) (bool, error) {
-	return statFileWith(t.Right, env, func(fi os.FileInfo) (bool, error) {
+	return statFileWith(t.Right, env, func(fi os.FileInfo) bool {
 		var (
 			perm  = fi.Mode().Perm()
 			owner = perm&0600 != 0
 			group = perm&0060 != 0
 			other = perm&0006 != 0
 		)
-		return owner || group || other, nil
+		return owner || group || other
 	})
 }
 
 func (t UnaryTest) fileWritable(env Environment) (bool, error) {
-	return statFileWith(t.Right, env, func(fi os.FileInfo) (bool, error) {
+	return statFileWith(t.Right, env, func(fi os.FileInfo) bool {
 		var (
 			perm  = fi.Mode().Perm()
 			owner = perm&0400 != 0
 			group = perm&0040 != 0
 			other = perm&0004 != 0
 		)
-		return owner || group || other, nil
+		return owner || group || other
 	})
 }
 
 func (t UnaryTest) fileSize(env Environment) (bool, error) {
-	return statFileWith(t.Right, env, func(fi os.FileInfo) (bool, error) {
-		return fi.Size() > 0, nil
+	return statFileWith(t.Right, env, func(fi os.FileInfo) bool {
+		return fi.Size() > 0
 	})
 }
 
 func (t UnaryTest) fileRegular(env Environment) (bool, error) {
-	return statFileWith(t.Right, env, func(fi os.FileInfo) (bool, error) {
-		return fi.Mode().IsRegular(), nil
+	return statFileWith(t.Right, env, func(fi os.FileInfo) bool {
+		return fi.Mode().IsRegular()
 	})
 }
 
 func (t UnaryTest) fileDirectory(env Environment) (bool, error) {
-	return statFileWith(t.Right, env, func(fi os.FileInfo) (bool, error) {
-		return fi.IsDir(), nil
+	return statFileWith(t.Right, env, func(fi os.FileInfo) bool {
+		return fi.IsDir()
 	})
 }
 
 func (t UnaryTest) fileLink(env Environment) (bool, error) {
-	return statFileWith(t.Right, env, func(fi os.FileInfo) (bool, error) {
-		return fi.Mode()&os.ModeSymlink == os.ModeSymlink, nil
+	return statFileWith(t.Right, env, func(fi os.FileInfo) bool {
+		return fi.Mode()&os.ModeSymlink == os.ModeSymlink
 	})
 }
 
 func (t UnaryTest) fileExec(env Environment) (bool, error) {
-	return statFileWith(t.Right, env, func(fi os.FileInfo) (bool, error) {
+	return statFileWith(t.Right, env, func(fi os.FileInfo) bool {
 		var (
 			perm  = fi.Mode().Perm()
 			owner = perm&0100 != 0
 			group = perm&0010 != 0
 			other = perm&0001 != 0
 		)
-		return owner || group || other, nil
+		return owner || group || other
 	})
 }
 
@@ -167,11 +177,29 @@ func (t BinaryTest) IsQuoted() bool {
 func (t BinaryTest) Test(env Environment) (bool, error) {
 	switch t.Op {
 	case Eq:
+		return t.compare(env, func(left, right string) bool {
+			return left == right
+		})
 	case Ne:
+		return t.compare(env, func(left, right string) bool {
+			return left != right
+		})
 	case Lt:
+		return t.compare(env, func(left, right string) bool {
+			return left < right
+		})
 	case Le:
+		return t.compare(env, func(left, right string) bool {
+			return left <= right
+		})
 	case Gt:
+		return t.compare(env, func(left, right string) bool {
+			return left > right
+		})
 	case Ge:
+		return t.compare(env, func(left, right string) bool {
+			return left >= right
+		})
 	case SameFile:
 		return t.sameFile(env)
 	case OlderThan:
@@ -179,8 +207,20 @@ func (t BinaryTest) Test(env Environment) (bool, error) {
 	case NewerThan:
 		return t.newerThan(env)
 	default:
+		return false, fmt.Errorf("unknown/unsupported unary test operator")
 	}
-	return false, ErrTest
+}
+
+func (t BinaryTest) compare(env Environment, cmp func(left, right string) bool) (bool, error) {
+	left, err := expandSingle(t.Left, env)
+	if err != nil {
+		return false, err
+	}
+	right, err := expandSingle(t.Right, env)
+	if err != nil {
+		return false, err
+	}
+	return cmp(left, right), nil
 }
 
 func (t BinaryTest) olderThan(env Environment) (bool, error) {
@@ -219,21 +259,29 @@ func (t BinaryTest) sameFile(env Environment) (bool, error) {
 	return os.SameFile(left, right), nil
 }
 
-func statFileWith(ex Expander, env Environment, check func(os.FileInfo) (bool, error)) (bool, error) {
+func statFileWith(ex Expander, env Environment, stat func(os.FileInfo) bool) (bool, error) {
 	fi, err := statFile(ex, env)
 	if err != nil {
 		return false, err
 	}
-	return check(fi)
+	return stat(fi), nil
 }
 
 func statFile(ex Expander, env Environment) (os.FileInfo, error) {
-	str, err := ex.Expand(env, true)
+	str, err := expandSingle(ex, env)
 	if err != nil {
 		return nil, err
 	}
-	if len(str) != 1 {
-		return nil, fmt.Errorf("%w: expected only 1 value to be expanded (got %dd)", ErrExpansion, len(str))
+	return os.Stat(str)
+}
+
+func expandSingle(ex Expander, env Environment) (string, error) {
+	str, err := ex.Expand(env, true)
+	if err != nil {
+		return "", err
 	}
-	return os.Stat(str[0])
+	if len(str) != 1 {
+		return "", fmt.Errorf("%w: expected only 1 value to be expanded (got %dd)", ErrExpansion, len(str))
+	}
+	return str[0], nil
 }
