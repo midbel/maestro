9f6cd63eb43091b69a328ff0e6872b759269cfb6
midbel <brun.nicolas21@gmail.com>
2022-01-14 19:16:04 +0100

    clean executeHost + rest

--- maestro.go
+++ maestro.go
@@ -1,7 +1,6 @@
 package maestro
 
 import (
-	"bufio"
 	"bytes"
 	"context"
 	"errors"
@@ -15,7 +14,6 @@ import (
 	"sort"
 	"strings"
 	"sync"
-	"time"
 
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/sync/errgroup"
@@ -80,6 +78,10 @@ func New() *Maestro {
 	}
 }
 
+func (m *Maestro) Name() string {
+	return strings.TrimSuffix(filepath.Base(m.File), filepath.Ext(m.File))
+}
+
 func (m *Maestro) Load(file string) error {
 	r, err := os.Open(file)
 	if err != nil {
@@ -95,6 +97,32 @@ func (m *Maestro) Load(file string) error {
 		return err
 	}
 	m.MetaAbout.File = file
+	return nil
+}
+
+func (m *Maestro) Register(cmd *Single) error {
+	curr, ok := m.Commands[cmd.Name]
+	if !ok {
+		m.Commands[cmd.Name] = cmd
+		return nil
+	}
+	switch m.Duplicate {
+	case dupError:
+		return fmt.Errorf("%s %w", cmd.Name, ErrDuplicate)
+	case dupReplace, "":
+		m.Commands[cmd.Name] = cmd
+	case dupAppend:
+		if mul, ok := curr.(Combined); ok {
+			curr = append(mul, cmd)
+			break
+		}
+		mul := make(Combined, 0, 2)
+		mul = append(mul, curr)
+		mul = append(mul, cmd)
+		m.Commands[cmd.Name] = mul
+	default:
+		return fmt.Errorf("DUPLICATE: unknown value %s", m.Duplicate)
+	}
 	return nil
 }
 
@@ -130,29 +158,6 @@ func (m *Maestro) Graph(name string) error {
 	return err
 }
 
-func (m *Maestro) traverseGraph(name string, level int) ([]string, error) {
-	cmd, err := m.lookup(name)
-	if err != nil {
-		return nil, err
-	}
-	s, ok := cmd.(*Single)
-	if !ok {
-		return nil, nil
-	}
-	fmt.Fprintf(stdout, "%s- %s", strings.Repeat(" ", level*2), name)
-	fmt.Fprintln(stdout)
-	var list []string
-	for _, d := range s.Deps {
-		others, err := m.traverseGraph(d.Name, level+1)
-		if err != nil {
-			return nil, err
-		}
-		list = append(list, others...)
-		list = append(list, d.Name)
-	}
-	return list, nil
-}
-
 func (m *Maestro) Schedule() error {
 	return nil
 }
@@ -162,7 +167,6 @@ func (m *Maestro) Dry(name string, args []string) error {
 	if err != nil {
 		return err
 	}
-	m.TraceCommand(cmd, args)
 	return cmd.Dry(args)
 }
 
@@ -259,32 +263,6 @@ func (m *Maestro) executeVersion(w io.Writer) error {
 	return nil
 }
 
-func (m *Maestro) Register(cmd *Single) error {
-	curr, ok := m.Commands[cmd.Name]
-	if !ok {
-		m.Commands[cmd.Name] = cmd
-		return nil
-	}
-	switch m.Duplicate {
-	case dupError:
-		return fmt.Errorf("%s %w", cmd.Name, ErrDuplicate)
-	case dupReplace, "":
-		m.Commands[cmd.Name] = cmd
-	case dupAppend:
-		if mul, ok := curr.(Combined); ok {
-			curr = append(mul, cmd)
-			break
-		}
-		mul := make(Combined, 0, 2)
-		mul = append(mul, curr)
-		mul = append(mul, cmd)
-		m.Commands[cmd.Name] = mul
-	default:
-		return fmt.Errorf("DUPLICATE: unknown value %s", m.Duplicate)
-	}
-	return nil
-}
-
 func (m *Maestro) executeRemote(cmd Command, args []string, stdout, stderr io.Writer) error {
 	scripts, err := cmd.Script(args)
 	if err != nil {
@@ -335,26 +313,23 @@ func (m *Maestro) executeHost(ctx context.Context, cmd Command, addr string, scr
 		perr.Close()
 	}()
 	exec := func(sess *ssh.Session, line string) error {
-		cmd.SetOut(pout.W)
-		cmd.SetErr(perr.W)
-
 		prefix := fmt.Sprintf("%s;%s;%s", m.MetaSSH.User, addr, cmd.Command())
+		pout.SetPrefix(prefix)
+		perr.SetPrefix(prefix)
 
-		go toStd(prefix, stdout, createLine(pout.R), m.WithPrefix)
-		go toStd(prefix, stderr, createLine(perr.R), m.WithPrefix)
+		go io.Copy(stdout, pout)
+		go io.Copy(stderr, perr)
 
 		defer sess.Close()
-		sess.Stdout = pout.W
-		sess.Stderr = perr.W
+		sess.Stdout = pout
+		sess.Stderr = perr
 
-		return m.TraceTime(cmd, nil, func() error {
-			return sess.Run(line)
-		})
+		return sess.Run(line)
 	}
 	config := ssh.ClientConfig{
 		User:            m.MetaSSH.User,
 		Auth:            m.MetaSSH.AuthMethod(),
-		HostKeyCallback: m.CheckHostKey, //ssh.InsecureIgnoreHostKey(),
+		HostKeyCallback: m.CheckHostKey,
 	}
 	client, err := ssh.Dial("tcp", addr, &config)
 	if err != nil {
@@ -376,16 +351,6 @@ func (m *Maestro) executeHost(ctx context.Context, cmd Command, addr string, scr
 		}
 	}
 	return nil
-}
-
-func (m *Maestro) executeList(ctx context.Context, list []string, stdout, stderr io.Writer) {
-	for i := range list {
-		cmd, err := m.prepare(list[i])
-		if err != nil {
-			continue
-		}
-		m.executeCommand(ctx, cmd, nil, stdout, stderr)
-	}
 }
 
 func (m *Maestro) help() (string, error) {
@@ -412,10 +377,6 @@ func (m *Maestro) help() (string, error) {
 	return renderTemplate(helptext, h)
 }
 
-func (m *Maestro) Name() string {
-	return strings.TrimSuffix(filepath.Base(m.File), filepath.Ext(m.File))
-}
-
 func (m *Maestro) canExecute(cmd Command) error {
 	if cmd.Blocked() {
 		return fmt.Errorf("%s: command can not be called", cmd.Command())
@@ -429,76 +390,10 @@ func (m *Maestro) canExecute(cmd Command) error {
 	return nil
 }
 
-func (m *Maestro) executeCommand(ctx context.Context, cmd Command, args []string, stdout, stderr io.Writer) error {
-	var (
-		pout, _ = createPipe()
-		perr, _ = createPipe()
-	)
-
-	defer func() {
-		pout.Close()
-		perr.Close()
-	}()
-
-	cmd.SetOut(pout.W)
-	cmd.SetErr(perr.W)
-
-	go toStd(cmd.Command(), stdout, createLine(pout.R), m.WithPrefix)
-	go toStd(cmd.Command(), stderr, createLine(perr.R), m.WithPrefix)
-
-	return m.TraceTime(cmd, args, func() error {
-		err := cmd.Execute(ctx, args)
-		if err != nil && m.MetaExec.Ignore {
-			err = nil
-		}
-		return err
-	})
-}
-
-func (m *Maestro) executeDependencies(ctx context.Context, cmd Command) error {
-	deps, err := m.resolveDependencies(cmd)
-	if err != nil {
-		return err
-	}
-	var (
-		grp  errgroup.Group
-		seen = make(map[string]struct{})
-	)
-	for i := range deps {
-		if _, ok := seen[deps[i].Name]; ok {
-			continue
-		}
-		seen[deps[i].Name] = struct{}{}
-
-		cmd, err := m.prepare(deps[i].Name)
-		if err != nil {
-			if deps[i].Optional {
-				continue
-			}
-			return err
-		}
-		if d := deps[i]; d.Bg {
-			grp.Go(func() error {
-				if err := m.executeDependencies(ctx, cmd); err != nil {
-					return err
-				}
-				m.executeCommand(ctx, cmd, d.Args, stdout, stderr)
-				return nil
-			})
-		} else {
-			err := m.executeCommand(ctx, cmd, d.Args, stdout, stderr)
-			if err != nil && !deps[i].Optional {
-				return err
-			}
-		}
-	}
-	return grp.Wait()
-}
-
 func (m *Maestro) resolve(cmd Command, args []string) (executer, error) {
 	var list deplist
 	if !m.NoDeps {
-		deps, err := m.resolveDependenciesBis(cmd)
+		deps, err := m.resolveDependencies(cmd)
 		if err != nil {
 			return nil, err
 		}
@@ -538,7 +433,7 @@ func (m *Maestro) resolveList(names []string) ([]Command, error) {
 	return list, nil
 }
 
-func (m *Maestro) resolveDependenciesBis(cmd Command) (deplist, error) {
+func (m *Maestro) resolveDependencies(cmd Command) (deplist, error) {
 	var (
 		traverse func(Command) (deplist, error)
 		seen     = make(map[string]struct{})
@@ -578,32 +473,6 @@ func (m *Maestro) resolveDependenciesBis(cmd Command) (deplist, error) {
 			set = append(set, ex)
 		}
 		return deplist(set), nil
-	}
-	return traverse(cmd)
-}
-
-func (m *Maestro) resolveDependencies(cmd Command) ([]Dep, error) {
-	var traverse func(Command) ([]Dep, error)
-
-	traverse = func(cmd Command) ([]Dep, error) {
-		s, ok := cmd.(*Single)
-		if !ok {
-			return nil, nil
-		}
-		var all []Dep
-		for _, d := range s.Deps {
-			c, err := m.lookup(d.Name)
-			if err != nil {
-				return nil, err
-			}
-			set, err := traverse(c)
-			if err != nil {
-				return nil, err
-			}
-			all = append(all, set...)
-			all = append(all, d)
-		}
-		return all, nil
 	}
 	return traverse(cmd)
 }
@@ -652,6 +521,29 @@ func (m *Maestro) lookup(name string) (Command, error) {
 	return nil, fmt.Errorf("%s: command not defined", name)
 }
 
+func (m *Maestro) traverseGraph(name string, level int) ([]string, error) {
+	cmd, err := m.lookup(name)
+	if err != nil {
+		return nil, err
+	}
+	s, ok := cmd.(*Single)
+	if !ok {
+		return nil, nil
+	}
+	fmt.Fprintf(stdout, "%s- %s", strings.Repeat(" ", level*2), name)
+	fmt.Fprintln(stdout)
+	var list []string
+	for _, d := range s.Deps {
+		others, err := m.traverseGraph(d.Name, level+1)
+		if err != nil {
+			return nil, err
+		}
+		list = append(list, others...)
+		list = append(list, d.Name)
+	}
+	return list, nil
+}
+
 type MetaExec struct {
 	WorkDir string
 	Dry     bool
@@ -665,43 +557,6 @@ type MetaExec struct {
 	After   []string
 	Error   []string
 	Success []string
-}
-
-func (m MetaExec) TraceTime(cmd Command, args []string, run func() error) error {
-	m.traceStart(cmd, args)
-	var (
-		now = time.Now()
-		err = run()
-	)
-	m.traceEnd(cmd, err, time.Since(now))
-	return err
-}
-
-func (m MetaExec) TraceCommand(cmd Command, args []string) {
-	m.traceStart(cmd, args)
-}
-
-func (m MetaExec) traceEnd(cmd Command, err error, elapsed time.Duration) {
-	if !m.Trace {
-		return
-	}
-	if err != nil {
-		fmt.Print("[maestro] fail")
-		fmt.Println()
-	}
-	fmt.Printf("[maestro] time: %.3fs", elapsed.Seconds())
-	fmt.Println()
-}
-
-func (m MetaExec) traceStart(cmd Command, args []string) {
-	if !m.Trace {
-		return
-	}
-	fmt.Printf("[maestro] %s", cmd.Command())
-	if len(args) > 0 {
-		fmt.Printf(": %s", strings.Join(args, " "))
-	}
-	fmt.Println()
 }
 
 type MetaAbout struct {
@@ -787,56 +642,6 @@ type help struct {
 	Commands map[string][]Command
 }
 
-type prefixWriter struct {
-	prefix string
-	inner  io.Writer
-}
-
-func createPrefix(prefix string, w io.Writer) io.Writer {
-	if prefix == "" {
-		prefix = "maestro"
-	}
-	return &prefixWriter{
-		prefix: fmt.Sprintf("[%s] ", prefix),
-		inner:  w,
-	}
-}
-
-func (w *prefixWriter) SetPrefix(prefix string) {
-	if prefix == "" {
-		w.prefix = prefix
-		return
-	}
-	w.prefix = fmt.Sprintf("[%s] ", prefix)
-}
-
-func (w *prefixWriter) Write(b []byte) (int, error) {
-	io.WriteString(w.inner, w.prefix)
-	return w.inner.Write(b)
-}
-
-type lineReader struct {
-	scan *bufio.Scanner
-}
-
-func createLine(r io.Reader) io.Reader {
-	return &lineReader{
-		scan: bufio.NewScanner(r),
-	}
-}
-
-func (r *lineReader) Read(b []byte) (int, error) {
-	if !r.scan.Scan() {
-		err := r.scan.Err()
-		if err == nil {
-			err = io.EOF
-		}
-		return 0, io.EOF
-	}
-	x := r.scan.Bytes()
-	return copy(b, append(x, '\n')), r.scan.Err()
-}
-
 type lockedWriter struct {
 	mu sync.Mutex
 	io.Writer
@@ -858,13 +663,6 @@ var (
 	stdout = createLock(os.Stdout)
 	stderr = createLock(os.Stderr)
 )
-
-func toStd(prefix string, w io.Writer, r io.Reader, with bool) {
-	if with && prefix != "" {
-		w = createPrefix(prefix, w)
-	}
-	io.Copy(w, r)
-}
 
 func hasHelp(args []string) bool {
 	as := make([]string, len(args))
