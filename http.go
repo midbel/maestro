957c054b9635f69df0a9dbeff1ed1f964768f9ec
midbel <brun.nicolas21@gmail.com>
2022-01-17 19:43:25 +0100

    refactor execute command from http handler func

--- http.go
+++ http.go
@@ -1,6 +1,7 @@
 package maestro
 
 import (
+	"errors"
 	"io"
 	"net/http"
 	"path"
@@ -20,11 +21,6 @@ const (
 	httpHdrTrailer = "Trailer"
 )
 
-func parseBool(str string) bool {
-	b, _ := strconv.ParseBool(str)
-	return b
-}
-
 func ServeCommand(mst *Maestro) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		// var (
@@ -33,30 +29,25 @@ func ServeCommand(mst *Maestro) http.Handler {
 		// )
 		var (
 			name   = path.Base(r.URL.Path)
-			option = ctreeOption{
-				NoDeps: parseBool(r.Header.Get(httpHdrNoDeps)),
-				Ignore: parseBool(r.Header.Get(httpHdrIgnore)),
-				Trace:  parseBool(r.Header.Get(httpHdrTrace)),
-				Prefix: parseBool(r.Header.Get(httpHdrPrefix)),
-			}
+			option = getOption(r)
 		)
 		w.Header().Set(httpHdrTrailer, httpHdrExit)
-
-		cmd, err := mst.prepare(name)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+		var (
+			err  = executeCommand(w, name, option, mst)
+			code int
+		)
+		switch {
+		case errors.Is(err, errNotFound):
+			code = http.StatusBadRequest
+		case errors.Is(err, errResolve):
+			code = http.StatusInternalServerError
+		default:
+		}
+		if code >= http.StatusBadRequest {
+			w.WriteHeader(code)
+			io.WriteString(w, err.Error())
 			return
 		}
-		ex, err := mst.resolve(cmd, nil, option)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if c, ok := ex.(io.Closer); ok {
-			defer c.Close()
-		}
-		err = ex.Execute(r.Context(), w, w)
-
 		exit := "ok"
 		if err != nil {
 			exit = err.Error()
@@ -75,14 +66,35 @@ func ServeDebug(mst *Maestro) http.Handler {
 
 func ServeAll(mst *Maestro) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-
+		
 	}
 	return http.HandlerFunc(fn)
 }
 
 func ServeDefault(mst *Maestro) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-
+		w.Header().Set(httpHdrTrailer, httpHdrExit)
+		var (
+			err  = executeCommand(w, mst.MetaExec.Default, getOption(r), mst)
+			code int
+		)
+		switch {
+		case errors.Is(err, errNotFound):
+			code = http.StatusBadRequest
+		case errors.Is(err, errResolve):
+			code = http.StatusInternalServerError
+		default:
+		}
+		if code >= http.StatusBadRequest {
+			w.WriteHeader(code)
+			io.WriteString(w, err.Error())
+			return
+		}
+		exit := "ok"
+		if err != nil {
+			exit = err.Error()
+		}
+		w.Header().Set(httpHdrExit, exit)
 	}
 	return http.HandlerFunc(fn)
 }
@@ -108,4 +120,43 @@ func serveRequest(h http.Handler) http.Handler {
 		h.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
+}
+
+func getOption(r *http.Request) ctreeOption {
+	return ctreeOption{
+		NoDeps: parseBool(r.Header.Get(httpHdrNoDeps)),
+		Ignore: parseBool(r.Header.Get(httpHdrIgnore)),
+		Trace:  parseBool(r.Header.Get(httpHdrTrace)),
+		Prefix: parseBool(r.Header.Get(httpHdrPrefix)),
+	}
+}
+
+func parseBool(str string) bool {
+	b, _ := strconv.ParseBool(str)
+	return b
+}
+
+var (
+	errNotFound = errors.New("command not found")
+	errResolve  = errors.New("fail to resolve dependencies")
+	errExecute  = errors.New("execution fail")
+)
+
+func executeCommand(w io.Writer, name string, option ctreeOption, mst *Maestro) error {
+	cmd, err := mst.prepare(name)
+	if err != nil {
+		return errNotFound
+	}
+	ex, err := mst.resolve(cmd, nil, option)
+	if err != nil {
+		return errResolve
+	}
+	if c, ok := ex.(io.Closer); ok {
+		defer c.Close()
+	}
+	err = ex.Execute(r.Context(), w, w)
+	if err != nil {
+		err = fmt.Errorf("%w %s: %s", errExecute, name, err)
+	}
+	return err
 }
