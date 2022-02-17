fc65e3eb6744c1164c4b32293728f67f3e8d0301
midbel <brun.nicolas21@gmail.com>
2022-02-17 16:02:00 +0100

    add b option

--- cmd/cron/main.go
+++ cmd/cron/main.go
@@ -4,18 +4,28 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/midbel/maestro/schedule"
 )
 
 func main() {
 	n := flag.Int("n", 5, "show n next scheduled time")
+	b := flag.String("b", "", "use base time to start schedule")
 	flag.Parse()
 
 	sched, err := schedule.Schedule(flag.Arg(0), flag.Arg(1), flag.Arg(2), flag.Arg(3), flag.Arg(4))
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(5)
+	}
+	if *b != "" {
+		w, err := time.Parse(time.RFC3339, *b)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(3)
+		}
+		sched.Reset(w)
 	}
 	for i := 0; i < *n; i++ {
 		next := sched.Next()
